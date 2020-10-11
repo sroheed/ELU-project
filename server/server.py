c30c49daf6e89626614d00d4787095c463930671
Sanderzhanni <hannis@tlu.ee>
2020-10-11 17:28:06 +0300

    python vomit

--- server/server.py
+++ server/server.py
@@ -11,10 +11,6 @@ app.config["DEBUG"] = True
 def index():
     return app.send_static_file("index.html")
 
-@app.route('/graph-without-posts')
-def graph_without_posts():
-    return app.send_static_file("index.html")
-
 @app.route('/api/graph', methods=['GET'])
 def home():
     jsonResp = {'jack': 4098, 'sape': 4139}
