2afa2b60c91dd5187aa1b7d98eb12d6218ad40d1
Sanderzhanni <hannis@tlu.ee>
2020-10-11 17:58:38 +0300

    server fix

--- server/server.py
+++ server/server.py
@@ -27,6 +27,7 @@ def api_id():
 
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', debug=False, port=os.environ.get('PORT', 80))
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
 
 #app.run()
